e4c7e29408e6903352002c323b49c8a070d4efa2
Brian Chong <bchong81@gmail.com>
2019-10-20 23:03:38 -0400

    submission Bite 5 @ codechalleng.es

---
+++ 5/names.py
@@ -0,0 +1,40 @@
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    result = []
+    for name in names:
+        if name.title() not in result:
+            result.append(name.title())
+    return result
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+
+    last_names = []
+    for name in names:
+        last_names.append(name.split()[-1])
+
+    return sorted(last_names)[-1]
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+
+    first_names = []
+    for name in names:
+        first_names.append(name.split()[0])
+
+    first_names.sort(key=len)
+
+    return first_names[0]
