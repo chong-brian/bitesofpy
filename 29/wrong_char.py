373a189cee80021d87df1acc34e754a0fb342ea4
Brian Chong <bchong81@gmail.com>
2019-10-26 18:54:37 -0400

    submission Bite 29 @ codechalleng.es

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -12,3 +12,15 @@ def get_index_different_char(chars):
         return chars.index(alphanumerics[0])
     else:
         return chars.index(non_alphanumerics[0])
+
+
+"""
+def get_index_different_char(chars):
+    matches, no_matches = [], []
+    for i, char in enumerate(chars):
+        if str(char).lower() in alphanumeric_chars:
+            matches.append(i)
+        else:
+            no_matches.append(i)
+    return matches[0] if len(matches) == 1 else no_matches[0]
+"""
